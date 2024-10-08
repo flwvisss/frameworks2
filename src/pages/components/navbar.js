aa355df4f69c3f6cc4b4d3bfd15e4fc697c7ff92
flwvisss <flavia.defendi+6V@estudante.ifms.edu.br>
2024-10-08 18:27:32 +0000

    Aula 08/10

--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -1,5 +1,19 @@
+import {Container, Nav, Navbar} from 'react-bootstrap';
 
-function Navbar() {
-    return <h1>Navbar</h1>
+export default function Menu() {
+  return (
+    <Navbar expand="lg" className="degrademenu">
+      <Container>
+        <Navbar.Brand href="/">6v</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
-export default Navbar;
+
