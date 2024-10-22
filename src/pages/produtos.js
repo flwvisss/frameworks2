d08176305e49f44d2d91ee12f3b5422bbb89820b
flwvisss <flavia.defendi+6V@estudante.ifms.edu.br>
2024-10-22 18:25:24 +0000

    aula 22/10

---
+++ src/pages/produtos.js
@@ -0,0 +1,46 @@
+import { Container } from "react-bootstrap"
+import Menu from "./components/navbar"
+import Footer from "./components/footer"
+
+export default function Produtos() {
+    return <>
+        <Menu />
+        <Container>
+            <h1>Produtos</h1>
+            <table class="table table-info table-stripped">
+                
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+
+                </table>
+            </table>
+        </Container>
+        <Footer />
+    </>
+}
