c9e3f042d8562545eb90379b7ecbeb9d3f811eec
flwvisss <flavia.defendi+6V@estudante.ifms.edu.br>
2024-12-05 21:21:34 +0000

    aula 05/12

--- src/pages/cardprod.js
+++ src/pages/cardprod.js
@@ -1,16 +1,15 @@
-export default function CardProd(props){
-
+export default function CardProd(props) {
     return <>
-
-    <div className="col-sm-6 mb-3 mb-sm-0">
-        <div className="card">
-        <div className="card-body">
-            <h5 className="card-title">{props.titulo}</h5>
-            <p className="card-text">{props.descricao}</p>
-            <a href="#" className="btn btn-primary">{props.preco}</a>
+              <div className="col text-center">
+            <div className="card h-100">
+                <div className="card-body">
+                    <h5 className="card-title">{props.titulo}</h5>
+                    <p className="card-text">{props.preco}</p>
+                </div>
+                <div className="card-footer">
+                    <small className="text-body-secondary">Last updated 3 mins ago</small>
+                </div>
+            </div>
         </div>
-        </div>
-    </div>
-
     </>
 }
--- src/pages/pesportes.js
+++ src/pages/pesportes.js
@@ -1,19 +1,34 @@
 import { Container } from "react-bootstrap";
 import Menu from "./components/navbar";
 import Footer from "./components/footer";
-import Cardprod from "./cardprod";
+import CardProd from "./cardprod";
 
-export default function Pesportes(params){
-    return<>
+export default function Pesportes({produtos}) {
+    return <>
         <Menu />
-        <Container className="boirder border-2 rounded border-primary col-sm-6 text-center">Lista de Produtos de Esportes</Container>
+        <Container className="border border-2 
+    rounded border-primary col-sm-6 text-center">
+            Lista de Produtos de Esportes
+        </Container>
         <Container>
-            <div className="row">
-                <Cardprod titulo={"Bola de Volei"} descricao={"bola"} preco={"$90.00"} />
-                <Cardprod titulo={"Bola de Futsal"} descricao={"bola"} preco={"$90.00"} />
-                <Cardprod titulo={"Bola de Handball"} descricao={"bola"} preco={"$90.00"} />
-            </div>
+          <div >
+          <h1>Produtos Esportes</h1>
+          <div className="row row-cols-1 row-cols-md-5 g-3">
+            {produtos.map(produto=>
+            <div key={produto.id}>
+                  <CardProd titulo={produto.titulo}
+                  descricao={produto.descricao}
+                  preco={produto.preco}/>
+                  </div>
+                )}
+          </div>
+          </div>
         </Container>
         <Footer />
     </>
+}
+export async function getServerSideProps() {
+    const res = await fetch('http://localhost:3000/api/produtos/data')
+    const repo = await res.json()
+    return { props: { produtos: repo } }
 }
