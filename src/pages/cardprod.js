d4749886ca665b624a035db822d9e1399f340f2b
flwvisss <flavia.defendi+6V@estudante.ifms.edu.br>
2024-12-10 17:45:18 +0000

    aula 10/12 out tolocate

--- src/pages/cardprod.js
+++ src/pages/cardprod.js
@@ -4,7 +4,7 @@ export default function CardProd(props) {
             <div className="card h-100">
                 <div className="card-body">
                     <h5 className="card-title">{props.titulo}</h5>
-                    <p className="card-text">{props.preco.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
+                    <p className="card-text">{props.preco}</p>
                 </div>
                 <div className="card-footer">
                     <small className="text-body-secondary">Last updated 3 mins ago</small>
